8804c8d2a55cb5fc8951f122e714d15d7e64168a
eacnscnj <eacnscnj@gmail.com>
2025-01-20 15:50:12 +0800

    print end

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  vmprintAdapter(pagetable, 0);
+}
+
+void
+vmprintAdapter(pagetable_t pagetable,int depth){
+  char *pre[] = {
+    "..",
+    ".. ..",
+    ".. .. ..",
+  };
+  for(int i = 0;i<512;i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      printf("%s%d: pte %p pa %p\n",pre[depth],i,pte,PTE2PA(pte));
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        pagetable_t nt = (pagetable_t)PTE2PA(pte);
+        vmprintAdapter(nt,depth+1);
+      }
+    }
+  }
+  return;
+}
